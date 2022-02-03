4d42ef5b371d6881d25ef02d6dc423cd095e9468
Moal <97049235@st.deltion.nl>
2022-02-03 14:40:57 +0100

    leeftijdgrenzen zijn klaar

--- leeftijdGrenzen.php
+++ leeftijdGrenzen.php
@@ -1,16 +1,20 @@
-<?php 
+<?php
+
 $leeftijd =readline("Geef je leeftijd: ");
-$ouderdanzestien = $leeftijd - 16;
-echo "je bent ".$ouderdanzestien. " jaar ouder dan 16 .\n";
-{echo "\n";} 
-$jongerdanvijftig = 50- $leeftijd ;
-echo "je bent " .$jongerdanvijftig." jaar jonger dan 50 .\n";
-{echo "\n";}
-$hetduurt= 67-$leeftijd;
-echo "Het duurt nog " .$hetduurt. " tot dat je 67 jaar wordt .\n";
-{ echo "\n";}
-{ echo "\n";}
-$tothonderd = 100-$leeftijd;
-echo "Het duurt nog ".$tothonderd. " jaren voordta je 100 word .\n";
-{ echo "\n";}
+if($leeftijd< 9 || $leeftijd >118)
+exit;
+
+if($leeftijd ==62){
+  
+    echo"waarschuwing nog 5 jaar voor dat u 67 wordt";
+    }
+
+
+elseif($leeftijd==10 ||$leeftijd==16 || $leeftijd==18 || $leeftijd==50 ||$leeftijd==67 ||$leeftijd==100 ){
+    echo"Gefeliciteerd ";
+if($leeftijd ==62){
+  
+echo"waarschuwing nog 5 jaar voor dat u 67 wordt";
+}
+}
 ?>
--- test.php
+++ test.php
@@ -1,6 +1,25 @@
 <?php
+
+$leeftijd =readline("Geef je leeftijd: ");
+if($leeftijd< 9 || $leeftijd >118)
+exit;
+
+if($leeftijd ==62){
+  
+    echo"waarschuwing nog 5 jaar voor dat u 67 wordt";
+    }
+
+
+elseif($leeftijd==10 ||$leeftijd==16 || $leeftijd==18 || $leeftijd==50 ||$leeftijd==67 ||$leeftijd==100 ){
+    echo"Gefeliciteerd ";
+if($leeftijd ==62){
+  
+echo"waarschuwing nog 5 jaar voor dat u 67 wordt";
+}
+}
+/*
 function test(){
     echo "het is een test";
 }
-test();
+test();*/
 ?>
